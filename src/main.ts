316b63744b6a933856ae6f72d6e3365c9440ca61
rifqifarelmuhammad <rifqifarel222@gmail.com>
2023-02-01 08:46:12 +0700

    mini update

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors({
     origin: [
       'http://localhost:3000', 
@@ -10,6 +11,10 @@ async function bootstrap() {
     ],
     credentials: true
   });
+  app.use((req:any, res:any, next:any) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  })
   await app.listen(8000);
 }
 bootstrap();
