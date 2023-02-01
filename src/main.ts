05b6de69183a98ceabbf3e6a79876c0bb7a251e5
rifqifarelmuhammad <rifqifarel222@gmail.com>
2023-02-01 09:48:04 +0700

    Mini update

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.enableCors({
-    origin: ['https://simple-todolist-fe.vercel.app', 'http://localhost:3000'],
-    allowedHeaders: '*',
+    origin: 'https://simple-todolist-fe.vercel.app',
+    allowedHeaders: ['content-type'],
+    credentials: true,
   });
   await app.listen(8000);
 }
