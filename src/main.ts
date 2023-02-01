4daa4c923b32fe746fe679bb79d586046237c45f
rifqifarelmuhammad <rifqifarel222@gmail.com>
2023-02-01 09:07:02 +0700

    Mini update

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, { cors: true });
   app.enableCors({
-    origin: ['https://simple-todolist-fe.vercel.app', 'http://localhost:3000'],
+    origin: 'http://localhost:3000',
     allowedHeaders: '*',
   });
   await app.listen(8000);
