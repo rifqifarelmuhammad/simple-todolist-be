275f12fbcd9dcbf53d58e4ea94a14dc31ca6d91f
rifqifarelmuhammad <rifqifarel222@gmail.com>
2023-02-01 08:34:57 +0700

    Mini update

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      'http://localhost:3000'
+    ],
+    methods: ["GET", "POST", "PUT", "PATCH"],
+    credentials: true
+  });
   await app.listen(8000);
 }
 bootstrap();
